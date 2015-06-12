a534734f6c2c4749d7fcf7427d387914fb7d837b
Deannamhenry <dmhenry@fullsail.edu>
2015-06-11 17:25:28 -0700

    Expression
    Test and File add

---
+++ Henry_Deanna_Expressions_Assignment/Javascript/Script_Expressions.js
@@ -0,0 +1,36 @@
+/**
+ * Deanna Henry
+ * Expression Assignment
+ * Section 02
+ * June 11, 2015.
+ */
+
+//tested to see if connected.);
+
+
+// Angle Calculator
+
+// Welcome User
+
+alert("Welcome to Triangle Calc!");
+
+// Ask user for info on angle
+
+var side1 = prompt("What is the degree of your first side?")
+console.log(side1);
+
+// Ask user for side 2 of angle
+
+var side2 = prompt("Please enter the second side ");
+console.log(side2);
+
+alert("Thank you for the information")
+
+//Calculation for the answer.
+var degree= 180
+
+var side3 = degree - side1 - side2;
+
+alert("The ")
+
+//test code

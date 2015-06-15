f2aa1699969057f8fdd4a65d14fe56c0c012cd16
Deannamhenry <dmhenry@fullsail.edu>
2015-06-14 20:06:26 -0700

    Practice
    Conditionals

---
+++ Pratctice Code/Java/Script.js
@@ -0,0 +1,26 @@
+// Practice java file for new codes or attempts
+
+// check to se if page is setup correctly
+
+//alert("Hi how are you doing?"); works
+
+
+//if/ else statement:conditionals
+
+ var miles = prompt("What is your speed?");
+console.log(miles);
+
+
+if (miles >= 100)
+ {
+    alert("Please slow your speed down!");
+}
+else {
+    alert("Please raise your speed up.");
+
+}
+
+// code give "Please slow down to ever answer
+
+// finally had the alert give right answers to speed input. Example 0-100 "PRYSU" 100+ "PSYSD"
+

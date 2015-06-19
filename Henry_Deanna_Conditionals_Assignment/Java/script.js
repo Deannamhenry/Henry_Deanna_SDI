df2c611b8be244104cbe888c0246fbefb660f09c
Deannamhenry <dmhenry@fullsail.edu>
2015-06-18 22:55:00 -0700

    Conditionals
    conditionals added and adjust meant in words and calculation.

--- Henry_Deanna_Conditionals_Assignment/Java/script.js
+++ Henry_Deanna_Conditionals_Assignment/Java/script.js
@@ -6,14 +6,41 @@
 
 //alert("Welcome") TEST
 
-alert("Welcome to Room Painter")
+alert("Welcome to Room Painter");
+
+// First clump of information for walls of same and different color
+
+var area = prompt ("How many square feet are you painting?");
+console.log(area);
+
+//First conditional
+
+if(area >= 1000)
+{
+    alert("Wow! You have a lot of painting to do?")
+
+}else
+{
+    alert("Great you dont have to buy a lot of paint.")
+}
+
+// Worked enterd 2000 gave me (wow) entered 400 gave me (great)
 
 var walls = prompt("How many walls will you be painting?");
 console.log(walls);
 
-var color = prompt("Will you be using the same color for all the walls?")
-console.log(color)
+var color = prompt("How many walls will be the another color?");
+console.log(color);
 
-if{
-    
-}
+// calculation of walls
+
+var samecolor = walls - color;
+console.log(samecolor);
+
+// walls (4) - color (1) = samecolor (3)
+//
+
+
+alert("Ok I have "+samecolor+" wall one color and "+color+" another. Great!");
+
+

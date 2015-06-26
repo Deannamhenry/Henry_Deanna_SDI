af49a84011fd305ba4064cb72973342bb5080c41
Deannamhenry <dmhenry@fullsail.edu>
2015-06-25 20:52:22 -0700

    Functions
    Adding in while

--- Henry_Deanna_Functions_Assignment/Js/Script.js
+++ Henry_Deanna_Functions_Assignment/Js/Script.js
@@ -9,4 +9,24 @@
 alert("Welcome to Whats's your Function?");
 //Connected to index
 
-var prompt=
+//asking user for basic info to get started.
+
+var ready= prompt("Are you ready? Please answer yes or no.");
+console.log(ready);
+
+var name= prompt("What is your name?");
+console.log(name)
+
+var height= prompt("What is your height")
+console.log(height)
+
+///adding in while statemnent in case user inputs other ir wrong info.
+
+while(height==="" || isNan(height)){
+    //ask again
+    if(height===""){
+        height= prompt("please input correct information in numbers.");
+
+}
+    height =
+console.log(height)
